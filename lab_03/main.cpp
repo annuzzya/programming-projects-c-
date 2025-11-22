8aac34456a859d7b5c613171c73034026d726d95
Anna Kucherova <kycherova07@gmail.com>
2025-11-22 16:20:36 +0200

    Create a namespace and store the number of actions for each set

---
+++ lab_03/main.cpp
@@ -0,0 +1,14 @@
+// variant8.cpp
+// C++20, hardcoded schedule for variant 8
+
+#include <iostream>
+#include <map>
+#include <memory>
+
+namespace variant8
+{
+    inline const std::map<char, int> counts{
+            {'a', 5}, {'b', 6}, {'c', 4}, {'d', 8}, {'e', 9},
+            {'f', 6}, {'g', 6}, {'h', 9}, {'i', 7}, {'j', 6}
+    };
+}
